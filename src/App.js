6afd3a74d3529d8085e5302b18921ce554e5ae46
komalcoding <komalcoding@gmail.com>
2023-02-12 15:28:19 +0530

    added counterApp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from"react"
 function App() {
+  const [input, setinput]=useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+   <div>
+    <div>{input}</div>
+    <div>
+      <h1>Counter App</h1></div>
+      <button onClick={()=>setinput(input+1)}>increase</button>
+      <button onClick={()=>setinput(input-1)}>decrease</button>
+      <button onClick={()=>setinput(0)}>reset</button>
+   </div>
   );
 }
 
